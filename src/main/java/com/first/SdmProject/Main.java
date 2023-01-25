d7db6ee1d187f579c05749deb96fb3bcd447f85b
Jalindar <jalindardhawale2016@gmail.com>
2023-01-25 10:43:24 +0530

    added java application demo files

---
+++ src/main/java/com/first/SdmProject/Main.java
@@ -0,0 +1,16 @@
+package com.first.SdmProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+		// Testing in the main method
+		}
+		public static boolean checkIfInputIsAnEvenNumber(int
+		i){
+		return i % 2 == 0;
+		}
+		
+	}
+
+
